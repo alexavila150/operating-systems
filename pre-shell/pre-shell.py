4dc44d4e2dde593028f4d0f63fccec5821410f77
Alex Avila <alexadrianavila@gmail.com>
2021-01-29 18:02:12 -0700

    finished assignment

--- pre-shell/pre-shell.py
+++ pre-shell/pre-shell.py
@@ -1,7 +1,27 @@
 import os
 
-def create_four_processes():
-    raise NotImplementedError
+class Runner:
+    def __init__(self):
+        self.process_ids = []
+        
+    def exec(self, args):
+        self.process_ids.append(os.fork())
 
-os.fork()
-print("I am a process")
+        if self.process_ids[-1] == 0:
+            os.execve('/usr/bin/' + args[0], args, os.environ)
+    
+    def wait_for_all_processes(self):
+        for id in self.process_ids:
+            os.waitpid(id, 0)
+
+def main():
+    runner = Runner()
+    runner.exec(['cat', '/proc/cpuinfo'])
+    runner.exec(['echo', 'Hello World'])
+    runner.exec(['python3', 'spinner.py', '1000000']) 
+    runner.exec(['uname', '-a'])
+    
+    runner.wait_for_all_processes()
+    runner.exec(['python3', 'spinner.py', '2000000'])
+
+main()
